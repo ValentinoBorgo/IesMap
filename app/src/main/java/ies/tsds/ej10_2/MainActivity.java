5c6c388e83fe8f32772570390cf236c9083c5429
Ies2022 <finosmj@gmail.com>
2022-09-17 08:15:51 -0300

    Cuarto commit de la rama priact (Agrega logica al boton salir)

--- app/src/main/java/ies/tsds/ej10_2/MainActivity.java
+++ app/src/main/java/ies/tsds/ej10_2/MainActivity.java
@@ -3,6 +3,8 @@ package ies.tsds.ej10_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activarBotones();
+    }
+
+    private void activarBotones() {
+        Button btSalir = (Button) findViewById(R.id.btSalir);
+        btSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                accionBotonSalir();
+            }
+        });
+    }
+
+    private void accionBotonSalir() {
+        finish();
     }
 }
